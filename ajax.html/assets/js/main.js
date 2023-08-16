333c95351c90900dcd791997537cb42eda3ad804
musamM921 <135432572+musamM921@users.noreply.github.com>
2023-08-16 14:04:37 +0400

    update4

--- ajax.html/assets/js/main.js
+++ ajax.html/assets/js/main.js
@@ -1,2 +1,6 @@
-
+document.addEventListener('DOMContentLoaded', function() {
+    const addButton = document.getElementById('addButton');
+    const taskInput = document.getElementById('taskInput');
+    const taskList = document.getElementById('taskList');
+})
   
